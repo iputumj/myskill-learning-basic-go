bd5904d0b10fc68f53945fe8c730bb076824123a
I Putu Mahendra Juniartha <iputu.mj@gmail.com>
2024-04-10 01:43:26 +0700

    const

--- variable.go
+++ variable.go
@@ -3,11 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	/**
+	 * A variable is a container for storing data, a variable has a data type, which is the type of data it holds.
+	 */
 	var firstName string = "my"
 	var lastName string = "skill"
-
-	address := "Bali"
+	city := "Bali"
 
 	fmt.Println("Hello", firstName, lastName)
-	fmt.Println(address)
+	fmt.Println(city)
+
+	/**
+	 * constants are similar to variables in that they are data storage containers, but the data cannot be changed after being assigned
+	 */
+	const country = "Indonesia"
+	// country = "Korea" //error
+
+	fmt.Println(country)
 }
